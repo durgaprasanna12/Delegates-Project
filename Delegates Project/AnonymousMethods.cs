a79b3c7c939f75c2c54f34ebe84246d0aad80998
Durga Prasanna <prasannadurga689@gmail.com>
2026-03-17 16:23:41 +0530

    Anonymous method added

---
+++ Delegates Project/AnonymousMethods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_Project
+{
+    public delegate string GreetingsDelegate(string name); //delegate declaration for static method
+    class AnonymousMethods
+    {
+
+        static void Main()
+        {
+            GreetingsDelegate obj = delegate (string name)
+            {
+                return "Hello " + name;
+            };
+            //creating delegate object for static method
+            string str = obj.Invoke("durga"); //calling static method using delegate
+            Console.WriteLine(str);
+            //creating delegate object for static method using anonymous method
+        }
+    }
+}
+      
