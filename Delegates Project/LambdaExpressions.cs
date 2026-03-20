18ff38f9090f41d86dab546b704df2d3cc38d949
Durga Prasanna <prasannadurga689@gmail.com>
2026-03-20 11:25:00 +0530

    Added lambda expressions code

---
+++ Delegates Project/LambdaExpressions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Delegates_Project
+{
+     class LambdaExpressions
+    {
+       
+        static void Main()
+        {
+            GreetingsDelegate obj = delegate (string name)
+            {
+                return "Hello" + name;
+            };
+            //creating delegate object for static method
+            string str=obj.Invoke("durga"); //calling static method using delegate
+            Console.WriteLine(str);
+
+        }
+    }
+}
